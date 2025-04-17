e32d5660d1724a4d9de265962c764f6ab51a4f3c
EC2 Default User <ec2-user@ip-172-31-92-254.ec2.internal>
2025-04-17 12:47:58 +0000

    added code for login page

--- dbCode.py
+++ dbCode.py
@@ -39,7 +39,7 @@ def get_list_of_dictionaries():
     query = "SELECT DISTINCT title, overview, release_date FROM movie, movie_company, production_company, movie_keywords, keyword WHERE movie.movie_id = movie_company.movie_id AND movie_company.company_id = production_company.company_id AND movie.movie_id = movie_keywords.movie_id AND movie_keywords.keyword_id = keyword.keyword_id AND (company_name = 'Walt Disney Animation Studios' OR company_name = 'Walt Disney Feature Animation' OR company_name = 'Walt Disney Pictures' OR company_name = 'Walt Disney Productions') AND keyword_name = 'princess' AND title <> 'Dragonslayer' AND title <> 'The Princess Diaries' AND title <> 'John Carter' AND title <> 'Into the Woods' AND title <> 'Enchanted' ORDER BY release_date ASC;"
     return execute_query_RDS(query)
 
-def print_voters():
+def get_voters():
     
     table = get_conn_Dynamo()
     response = table.scan()
--- flaskapp.py
+++ flaskapp.py
@@ -11,9 +11,29 @@ app.secret_key = 'your_secret_key' # this is an artifact for using flash display
 @app.route('/')
 def home():
     movies_list = get_list_of_dictionaries()
-    voter = print_voters()
-    print(movies_list)
+    voter = get_voters()
     return render_template('home.html', movies = movies_list, voters = voter)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        # Extract form data
+        username = request.form['username']
+        password = request.form['password']
+
+        response = (get_conn_Dynamo()).scan()
+        for person in response["Items"]:
+            if (person['Username'] == username and person['Password'] == password):
+                
+                flash('User added successfully!', 'success') 
+            else:
+
+                flash('Something was wrong and we could not log you in, please try again!', 'warning')
+        # Redirect to home page or another page upon successful submission
+        return redirect(url_for('home'))
+    else:
+        # Render the form page if the request method is GET
+        return render_template('login.html')
 
 
 if __name__ == '__main__':
